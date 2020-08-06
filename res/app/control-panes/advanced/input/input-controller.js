87992be683f0aea4cd8ae4ca719aff24b285ff4a
David Helkowski <git@codechild.com>
2020-08-06 18:12:58 +0000

    Alert response functionality
    The advanced section has a new button now to show a dialog for responding to an on-screen alert.
    
    Initial mouse wheel support for scrolling screen vertically has been added, but it has issues. If you scroll more than a single "notch" at a time it can freeze the WDA.
    
    For now have added in console logging of messages via the websocket to the backend. This makes events far easier to debug and also generally see what is happening.

--- res/app/control-panes/advanced/input/input-controller.js
+++ res/app/control-panes/advanced/input/input-controller.js
@@ -1,6 +1,56 @@
-module.exports = function InputCtrl($scope) {
+import MicroModal from 'micromodal';//require('micromodal');
+import DomCascade from 'dom-cascade';
+import MinLib from 'minlib';
 
+export default function InputCtrl($scope) {
   $scope.press = function(key) {
     $scope.control.keyPress(key)
+  }
+  
+  $scope.handle_alert = function() {
+    MicroModal.init();
+    
+    var dc = new DomCascade();
+    var ml = new MinLib();
+    
+    $scope.control.getAlertInfo().then( function( arr ) {
+      //<button class="modal__btn modal__btn-primary">Continue</button>
+      //    <button class="modal__btn" data-micromodal-close aria-label="Close this dialog window">Close</button>
+          
+      var text = arr[0].body.value;
+      var parts = text.split('\n');
+      var title = parts.shift();
+      ml.gel('alertModal-title').innerHTML = title;
+      
+      var dcParts = [];
+      for( var i=0;i<parts.length;i++ ) {
+          dcParts.push( { name: 'text', text: parts[i] } );
+          if( i != ( parts.length - 1 ) ) dcParts.push( { name: 'br' } );
+      }
+      if( dcParts.length ) dc.replaceInner( ml.gel('alertModal-content'), dcParts );
+      
+      var btns = arr[1].body.value;
+      var dcCode = [];
+      for( var i=0;i<btns.length;i++ ) {
+        var btn = btns[i];
+        dcCode.push( {
+          name: 'button',
+          class: ( (i==0) ? 'modal__btn modal__btn-primary' : 'modal__btn' ),
+          attr: { 'data-micromodal-close': 1, 'data-text': btn },
+          sub: { name: 'text', text: btn }
+        } );
+      }
+      dc.replaceInner( ml.gel('alertModal-footer'), dcCode );
+      
+      MicroModal.show('alertModal', {
+        onClose: function( a, b, event ) {
+            var btn = event.originalTarget;
+            var btnText = btn.getAttribute('data-text')
+            console.log( btnText );
+            $scope.control.alertAccept( btnText );
+        }
+      });
+      
+    } );
   }
 }
